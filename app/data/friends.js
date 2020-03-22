7fdbc53bfe65b3d1ef555caff014a05e2c460a3d
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-03-22 16:48:44 -0400

    editing done

--- app/data/friends.js
+++ app/data/friends.js
@@ -41,14 +41,14 @@ var answersArray = [
         "scores": [
             5,
             1,
+            1,
             4,
             4,
-            5,
             1,
             2,
-            5,
+            3,
             4,
-            1
+            4
         ]
     },
 
@@ -62,333 +62,256 @@ var answersArray = [
             3,
             2,
             1,
-            3,
-            4,
-            3,
-            2
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            1,
-            1,
-            2,
-            3,
-            2,
-            5,
             2,
             4,
-            2,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            4,
-            3,
-            4,
-            3,
-            4,
-            3,
-            3,
-            1,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            1,
-            1,
-            4,
-            1,
-            1,
-            1,
-            1,
-            2,
-            2,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            5,
-            3,
-            3,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            1,
-            2,
-            1,
-            1,
-            1,
-            1,
-            1,
-            2,
-            1,
-            1
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            1,
-            1,
-            1,
-            1,
-            1,
-            3,
-            3,
-            3,
             3,
             3
         ]
     },
     {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "name": "Sarah",
+        "photo": "http://www.liketotally80s.com/wp-content/uploads/2014/06/80s-eyebrows-brooke-shields-480px.png",
         "scores": [
-            3,
-            3,
-            3,
-            3,
-            3,
-            4,
-            3,
+            2,
+            5,
+            5,
             4,
             4,
+            4,
+            3,
+            3,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Melissa",
+        "photo": "https://i.pinimg.com/originals/fa/c8/84/fac88492ddee33c046cfac78f05a9e22.jpg",
+        "scores": [
+            1,
+            1,
+            5,
+            5,
+            5,
+            5,
+            4,
+            3,
+            2,
             3
         ]
     },
     {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "name": "Nikki",
+        "photo": "https://i.pinimg.com/236x/3e/0b/92/3e0b92c1ee60f219b137b356e4c88765--crazy-eyebrows-s-grunge.jpg",
         "scores": [
-            2,
-            2,
-            4,
-            2,
-            3,
-            2,
-            3,
-            2,
-            3,
-            2
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            3,
-            3,
-            2,
-            3,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2
-        ]
-    },
-    {
-        "name": "Gina",
-        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
-        "scores": [
-            2,
-        1,
-            4,
             5,
             4,
-            4,
             2,
+            4,
             3,
             2,
-            4
+            2,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Heather",
+        "photo": "http://2.bp.blogspot.com/-jz5sz-n3q-k/T0prjtbIktI/AAAAAAAAAJA/k3H9k1bI3kY/s1600/madonna.jpg",
+        "scores": [
+            1,
+            5,
+            4,
+            3,
+            2,
+            2,
+            2,
+            4,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Tiffany",
+        "photo": "https://lh5.googleusercontent.com/-7-Z-tXLWrfQ/TYBOLHo4GwI/AAAAAAAAEIM/e27Oq8rU50I/s1600/awesome+80s+hair.jpg",
+        "scores": [
+            1,
+            4,
+            4,
+            3,
+            3,
+            3,
+            3,
+            4,
+            3,
+            1
+        ]
+    },
+   
+    {
+        "name": "Michelle",
+        "photo": "https://media.allure.com/photos/5771a8d42554df47220a6c50/master/w_727,h_727,c_limit/hair-ideas-2015-10-80s-hair-teenage-girl.jpg",
+        "scores": [
+            1,
+            2,
+            1,
+            2,
+            1,
+            1,
+            2,
+            1,
+            2,
+            1
         ]
     },
 
+    {
+        "name": "Michael",
+        "photo": "http://4.bp.blogspot.com/-3xLzuj_xkHw/ThU6ElwIiqI/AAAAAAAAACY/afscg_04tqI/s1600/Mens_medium_hair_style_with_long_sidepart_bangs_black.319144454_large.jpg",
+        "scores": [
+            1,
+            3,
+            1,
+            3,
+            1,
+            3,
+            1,
+            3,
+            1,
+            1
+        ]
+    },
+
+    {
+        "name": "Matthew",
+        "photo": "https://www.teamjimmyjoe.com/wp-content/uploads/2013/07/Ryan-Newman.jpg",
+        "scores": [
+            1,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "James",
+        "photo": "https://s-media-cache-ak0.pinimg.com/236x/f6/a7/91/f6a791341c21e5a6b296c2c35290240b.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "David",
+        "photo": "https://i.pinimg.com/originals/58/df/ee/58dfeeaf49fbb8f464dab53d31e19447.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Christopher",
+        "photo": "https://s-media-cache-ak0.pinimg.com/736x/ec/8e/2f/ec8e2f19c6dbb57df6ea80e903e95778.jpg",
+        "scores": [
+            5,
+            4,
+            3,
+            4,
+            3,
+            4,
+            3,
+            3,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Robert",
+        "photo": "https://cdn.shopify.com/s/files/1/0197/7436/products/wigs-blonde-men-s-spiky-mullet-wig-fancy-dress-1980s-costume-party-punk-rock-hair-1.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            1,
+            1,
+            1,
+            1,
+            2,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "John",
+        "photo": "http://alivehaircuts.com/wp-content/uploads/2016/12/Top-20-Inspiring-Black-Men-Hairstyles.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Joseph",
+        "photo": "https://images.esellerpro.com/2372/I/211/800/FDF_BW693.jpg",
+        "scores": [
+            5,
+            3,
+            3,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Andrew",
+        "photo": "http://i.ebayimg.com/images/i/371731111125-0-1/s-l1000.jpg",
+        "scores": [
+            1,
+            2,
+            1,
+            1,
+            1,
+            1,
+            1,
+            2,
+            1,
+            1
+        ]
+    },
+    
 
 
 ]
