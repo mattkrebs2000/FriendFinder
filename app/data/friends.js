c359b45a2ab7d38928a0fac14fff070f8ae6b83e
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-03-22 12:43:59 -0400

    new commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 
 var answersArray = [
 {
-    "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
             "scores": [
                 5,
                 1,
@@ -15,7 +15,382 @@ var answersArray = [
                 4,
                 1
             ]
-}
+},
+
+{
+        "name": "Jennifer",
+    "photo": "https://childinsider.com/wp-content/uploads/2019/04/80s-hairstyles-for-girls-4.jpg",
+        "scores": [
+            3,
+            2,
+            3,
+            4,
+            5,
+            1,
+            1,
+            3,
+            2,
+            5
+        ]
+    },
+
+
+{
+        "name": "Sarah",
+    "photo": "http://www.liketotally80s.com/wp-content/uploads/2014/06/80s-eyebrows-brooke-shields-480px.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        "name": "Amanda",
+        "photo": "https://i.pinimg.com/originals/da/d2/8a/dad28a8125d25f3a97b94c09b770cfcb.jpg",
+        "scores": [
+            2,
+            3,
+            2,
+            3,
+            2,
+            1,
+            3,
+            4,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            1,
+            1,
+            2,
+            3,
+            2,
+            5,
+            2,
+            4,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            4,
+            3,
+            4,
+            3,
+            4,
+            3,
+            3,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            1,
+            1,
+            1,
+            1,
+            2,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            5,
+            3,
+            3,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            1,
+            2,
+            1,
+            1,
+            1,
+            1,
+            1,
+            2,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            4,
+            3,
+            4,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            2,
+            2,
+            4,
+            2,
+            3,
+            2,
+            3,
+            2,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            3,
+            3,
+            2,
+            3,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Gina",
+        "photo": "https://i.pinimg.com/originals/40/25/58/40255870b52e6d6f4e75bb0c2d2f7e56.jpg",
+        "scores": [
+            2,
+        1,
+            4,
+            5,
+            4,
+            4,
+            2,
+            3,
+            2,
+            4
+        ]
+    },
+
+
+
 ]
 
 module.exports = answersArray; 
